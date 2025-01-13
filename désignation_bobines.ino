5afd16beab1fe66e994866194eadf3671bd85b7e
Nada-mhbk <nada.al-mharzi@outlook.com>
2025-01-13 14:13:21 +0100

    RIEN A CHANGER
    RIEN

--- désignation_bobines.ino
+++ désignation_bobines.ino
@@ -3,34 +3,50 @@
 
 
 //Fonction qui donne une valeur à chacun des 6 bits en fonction de l'adresse saisie
-void setAddress(int address) {
+//address est un entier représentant une adresse sur 6 bits (0 à 63)
+//Chaque bit de address correspond à l’état d’une broche (HIGH ou LOW).
+
+//Objectif : Configurer une adresse sur les multiplexeurs, avec le bit de poids le plus fort représentant l'état D (allumer/éteindre la bobine).
+//Elle retourne une adresse de 7 bits.
+
+
+int setAddress(int address, bool d) {
+
+    //Ajouter l'état D dans le bit de poids le plus fort (bit 6)
+     if (d) {
+        address |= 0x40;  // Mettre le bit 6 à 1 si D = 1 (bobine allumée)
+    } else {
+        address &= ~0x40; // Mettre le bit 6 à 0 si D = 0 (bobine éteinte)
+    }
+
+
     // Configurer les broches a1 à a6 en fonction de l'adresse (6 bits)
-    digitalWrite(A1_PIN, (address & 0x01));      // Bit 0
-    digitalWrite(A2_PIN, (address & 0x02) >> 1); // Bit 1
-    digitalWrite(A3_PIN, (address & 0x04) >> 2); // Bit 2
-    digitalWrite(A4_PIN, (address & 0x08) >> 3); // Bit 3
-    digitalWrite(A5_PIN, (address & 0x10) >> 4); // Bit 4
-    digitalWrite(A6_PIN, (address & 0x20) >> 5); // Bit 5
+    digitalWrite(A1_PIN, (address & 0x01));      //Envoie le Bit 0 de address à la broche A1_PIN
+    digitalWrite(A2_PIN, (address & 0x02) >> 1); // Bit 1 à la broche A2_PIN
+    digitalWrite(A3_PIN, (address & 0x04) >> 2); // Bit 2 à la broche A3_PIN
+    digitalWrite(A4_PIN, (address & 0x08) >> 3); // Bit 3 à la broche A4_PIN
+    digitalWrite(A5_PIN, (address & 0x10) >> 4); // Bit 4 à la broche A5_PIN
+    digitalWrite(A6_PIN, (address & 0x20) >> 5); // Bit 5 à la broche A6_PIN
+
+    
+    // Retourner l'adresse complète (7 bits)
+    return adress;
 }
 
-digitalWrite(ENABLE_PIN, HIGH); // Activer le multiplexeur
-digitalWrite(D_PIN, HIGH);     // Allumer la bobine via la bascule D
-
-digitalWrite(ENABLE_PIN, HIGH); // Activer le multiplexeur
-digitalWrite(D_PIN, LOW);      // Éteindre la bobine via la bascule 
 
 
 void setBobine(int address, char Etat) {
-    setAddress(address);  // Configurer l'adresse sur les multiplexeurs
-
     switch (Etat) {
         case 'A':  // Activer
+            setAddress(address, true);  // Allumer la bobine
             activerBobine();
             break;
         case 'D':  // Désactiver
+            setAddress(address, false); // Éteindre la bobine
             desactiverBobine();
             break;
         default:
             Serial.println("Commande invalide !");
-            break;
-    }
+            break;
+    }
+}
