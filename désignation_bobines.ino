dfd512fe68ede575daaa1a98dcb3d38eb473ee41
Nada-mhbk <nada.al-mharzi@outlook.com>
2025-01-13 16:28:37 +0100

    Update désignation_bobines.ino

--- désignation_bobines.ino
+++ désignation_bobines.ino
@@ -1,5 +1,12 @@
 
 
+#define Delais 1000  // Délai en millisecondes
+#define GPIO1 D11 
+#define GPIO2 D10
+#define GPIO3 D9
+#define GPIO4 D6
+
+int GPIO[9] = {GPIO1, GPIO2, GPIO3, GPIO4, GPIO5, GPIO6, GPIO7, GPIO8};
 
 
 //Fonction qui donne une valeur à chacun des 6 bits en fonction de l'adresse saisie
@@ -12,23 +19,20 @@
 
 void setAddress(unsigned char address, bool d) {
 
-    address &= 0x7F; //on force le bit 7 à 0 puisque unsigned char est codé sur un octet = 8bits; ce bit est inutilisé
-
-    //Ajouter l'état D dans le bit 6
-     if (d) {
-        address |= 0x40;  // Mettre le bit 6 à 1 si D = 1 (bobine allumée)
+    if (d) {
+        address |= 0x80;  // Mettre le bit 7 à 1 si D = 1 (bobine allumée)
     } else {
-        address &= ~0x40; // Mettre le bit 6 à 0 si D = 0 (bobine éteinte)
+        address &= 0x7F;  // Mettre le bit 7 à 0 si D = 0 (bobine éteinte)
     }
 
 
     // Configurer les broches a1 à a6 en fonction de l'adresse (6 bits)
-    digitalWrite(A1_PIN, (address & 0x01));      //Envoie le Bit 0 de address à la broche A1_PIN
-    digitalWrite(A2_PIN, (address & 0x02) >> 1); // Bit 1 à la broche A2_PIN
-    digitalWrite(A3_PIN, (address & 0x04) >> 2); // Bit 2 à la broche A3_PIN
-    digitalWrite(A4_PIN, (address & 0x08) >> 3); // Bit 3 à la broche A4_PIN
-    digitalWrite(A5_PIN, (address & 0x10) >> 4); // Bit 4 à la broche A5_PIN
-    digitalWrite(A6_PIN, (address & 0x20) >> 5); // Bit 5 à la broche A6_PIN
+    digitalWrite(GPIO[0], (address & 0x01));      //Envoie le Bit 0 de address à la broche A1_PIN
+    digitalWrite(GPIO[1], (address & 0x02) >> 1); // Bit 1 à la broche A2_PIN
+    digitalWrite(GPIO[2], (address & 0x04) >> 2); // Bit 2 à la broche A3_PIN
+    digitalWrite(GPIO[3], (address & 0x08) >> 3); // Bit 3 à la broche A4_PIN
+    digitalWrite(GPIO[4], (address & 0x10) >> 4); // Bit 4 à la broche A5_PIN
+    digitalWrite(GPIO[5], (address & 0x20) >> 5); // Bit 5 à la broche A6_PIN
 
 }
 
@@ -36,11 +40,11 @@ void setAddress(unsigned char address, bool d) {
 //D_PIN est le signal de contrôle qui indique si la bobine doit être alimentée (HIGH) ou coupée (LOW).
 
 void activerBobine() {
-    digitalWrite(D_PIN, HIGH);  // Allumer la bobine sélectionnée
+    digitalWrite(GPIO[6], HIGH);  // Allumer la bobine sélectionnée
 }
 
 void desactiverBobine() {
-    digitalWrite(D_PIN, LOW);  // Éteindre la bobine sélectionnée
+    digitalWrite(GPIO[6], LOW);  // Éteindre la bobine sélectionnée
 }
 
 
